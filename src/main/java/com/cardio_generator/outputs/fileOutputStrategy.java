cdb671fb37921edc89c8bdd8b244a177965b1cde
AnoushkaRam <109474370+AnoushkaRam@users.noreply.github.com>
2024-04-28 09:05:06 +0200

    Corrected violations to align with Google Java Style Guide

--- src/main/java/com/cardio_generator/outputs/fileOutputStrategy.java
+++ src/main/java/com/cardio_generator/outputs/fileOutputStrategy.java
@@ -9,9 +9,10 @@ import java.util.concurrent.ConcurrentHashMap;
 
 public class fileOutputStrategy implements OutputStrategy {
 
-    private String BaseDirectory;
+    private String baseDirectory; //corrected variable's name to stick to the camelCase connvention thing
 
-    public final ConcurrentHashMap<String, String> file_map = new ConcurrentHashMap<>();
+    // access modifier changed for the encapsulation to be exact
+    private final ConcurrentHashMap<String, String> file_map = new ConcurrentHashMap<>(); // coorected for camelCase convention
 
     public fileOutputStrategy(String baseDirectory) {
 
@@ -22,17 +23,16 @@ public class fileOutputStrategy implements OutputStrategy {
     public void output(int patientId, long timestamp, String label, String data) {
         try {
             // Create the directory
-            Files.createDirectories(Paths.get(BaseDirectory));
+            Files.createDirectories(Paths.of(baseDirectory)); // cchanged Files.createDirectories to use Path.of for better readability and exception handling
         } catch (IOException e) {
             System.err.println("Error creating base directory: " + e.getMessage());
             return;
         }
         // Set the FilePath variable
-        String FilePath = file_map.computeIfAbsent(label, k -> Paths.get(BaseDirectory, label + ".txt").toString());
-
+        String filePath = file_map.computeIfAbsent(label, k -> Paths.get(BaseDirectory, label + ".txt").toString());  //changed the FilePath to camelCase
         // Write the data to the file
         try (PrintWriter out = new PrintWriter(
-                Files.newBufferedWriter(Paths.get(FilePath), StandardOpenOption.CREATE, StandardOpenOption.APPEND))) {
+                Files.newBufferedWriter(Paths.of(filePath), StandardOpenOption.CREATE, StandardOpenOption.APPEND))) {  //changed Files.newBufferedWriter to use Path.of for better readability and exception handling
             out.printf("Patient ID: %d, Timestamp: %d, Label: %s, Data: %s%n", patientId, timestamp, label, data);
         } catch (Exception e) {
             System.err.println("Error writing to file " + FilePath + ": " + e.getMessage());
