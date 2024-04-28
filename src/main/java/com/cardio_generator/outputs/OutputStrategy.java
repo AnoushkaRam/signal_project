8f6ddc9ad70381dab1161c6387ab428828ced478
AnoushkaRam <109474370+AnoushkaRam@users.noreply.github.com>
2024-04-28 09:42:58 +0200

    added comments

--- src/main/java/com/cardio_generator/outputs/OutputStrategy.java
+++ src/main/java/com/cardio_generator/outputs/OutputStrategy.java
@@ -1,5 +1,11 @@
+//An interface representing a strategy for outputting patient data in health monitoring simulations.
 package com.cardio_generator.outputs;
 
 public interface OutputStrategy {
+    //Outputs patient data to the specified destination.
     void output(int patientId, long timestamp, String label, String data);
+    //param patientId  = The ID of the patient associated with the data.
+    //@param timestamp  = The timestamp indicating when the data was recorded.
+    //@param label =  The label or type of data being output.
+    //@param data  =  The actual data to be output.
 }
