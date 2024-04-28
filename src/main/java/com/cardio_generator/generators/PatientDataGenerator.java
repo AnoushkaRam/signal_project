1d28aba11ba9b6bf6eba1034a9afd727793fb276
AnoushkaRam <109474370+AnoushkaRam@users.noreply.github.com>
2024-04-28 09:40:35 +0200

    added comments

--- src/main/java/com/cardio_generator/generators/PatientDataGenerator.java
+++ src/main/java/com/cardio_generator/generators/PatientDataGenerator.java
@@ -1,7 +1,12 @@
+//An interface for generating patient data in health monitoring simulations.
+
 package com.cardio_generator.generators;
 
 import com.cardio_generator.outputs.OutputStrategy;
 
 public interface PatientDataGenerator {
+    //Generates patient data based on the provided patient ID and output strategy.
     void generate(int patientId, OutputStrategy outputStrategy);
+    //@param patientId      The ID of the patient for which data is generated.
+    //@param outputStrategy The strategy for outputting the generated data.
 }
